914fabe394c1062b912544053aa88199462e4441
oyurekten <ozgury@ebi.ac.uk>
2026-03-20 14:41:48 +0000

    fix: upgrade mhd-model dependency and use validate_mhd_model method for validation

--- scripts/create_mhd_file.py
+++ scripts/create_mhd_file.py
@@ -1,15 +1,8 @@
-import json
 import logging
 from pathlib import Path
 
 import jsonschema
-from mhd_model.convertors.announcement.v0_1.legacy.mhd2announce import (
-    create_announcement_file,
-)
-from mhd_model.model.v0_1.announcement.validation.validator import (
-    MhdAnnouncementFileValidator,
-)
-from mhd_model.model.v0_1.dataset.validation.validator import validate_mhd_file
+from mhd_model.model.v0_1.dataset.validation.validator import validate_mhd_model
 
 from mw2mhd.config import (
     Mw2MhdConfiguration,
@@ -28,8 +21,6 @@ def convert_mw_study_to_mhd_legacy(
 ) -> tuple[bool, dict[str, list[jsonschema.ValidationError]]]:
     if not mw2mhd_config:
         mw2mhd_config = Mw2MhdConfiguration()
-    all_validation_errors = {}
-    success = False
     factory = Mw2MhdConvertorFactory()
 
     convertor = factory.get_convertor(
@@ -47,52 +38,7 @@ def convert_mw_study_to_mhd_legacy(
         mhd_output_filename=mhd_output_filename,
     )
     mhd_file_path = mhd_output_root_path / Path(mhd_output_filename)
-
-    validation_errors = validate_mhd_file(str(mhd_file_path))
-
-    if validation_errors:
-        logger.error("MHD model validation errors found for %s", mw_study_id)
-        for error in validation_errors:
-            logger.error(error)
-        all_validation_errors[mhd_output_filename] = validation_errors
-    elif not mhd_file_path.exists():
-        logger.error("MHD model file not found for %s", mw_study_id)
-        all_validation_errors[mhd_output_filename] = [
-            f"MHD model file '{mhd_output_filename}' not found"
-        ]
-    else:
-        mhd_announcement_output_root_path = Path("tests/mhd_announcement/legacy")
-        mhd_announcement_output_root_path.mkdir(exist_ok=True, parents=True)
-        logger.info("MHD model validation successful for %s", mw_study_id)
-        announcement_file_name = f"{study_id}.announcement.json"
-        announcement_file_path = mhd_announcement_output_root_path / Path(
-            announcement_file_name
-        )
-        mhd_data_json = json.loads(mhd_file_path.read_text())
-        mhd_file_url = mw2mhd_config.study_http_base_url + mw_study_id
-        create_announcement_file(mhd_data_json, mhd_file_url, announcement_file_path)
-        if not announcement_file_path.exists():
-            logger.error("MHD announcement file not found for %s", mw_study_id)
-            all_validation_errors[mhd_output_filename] = [
-                f"MHD announcement file '{mhd_output_filename}' not found"
-            ]
-        else:
-            announcement_file_json = json.loads(announcement_file_path.read_text())
-            validator = MhdAnnouncementFileValidator()
-            all_errors = validator.validate(announcement_file_json)
-            if all_errors:
-                logger.error(
-                    "MHD announcement file validation errors found for %s", mw_study_id
-                )
-                for error in all_errors:
-                    logger.error(error)
-                all_validation_errors[mhd_output_filename] = all_errors
-            else:
-                success = True
-                logger.info(
-                    "MHD announcement file validation successful for %s", mw_study_id
-                )
-    return success, all_validation_errors
+    return validate_mhd_model(mw_study_id, mhd_file_path, )
 
 
 if __name__ == "__main__":
